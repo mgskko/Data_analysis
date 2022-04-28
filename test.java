8ebf1398e5c6bc98c804664a1098e4cebef87420
mgskko <msk724012@gmail.com>
2022-04-28 14:22:28 +0900

    second

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        String[] arr = {"명석","유빈","진혁"};
+        for(int i =0 ; i < 3;i++){
+            System.out.println(arr[i]);
+
+        }
+        
+        }
+}
