7840ab540fc53d0815c645c95a272ab2534169ec
mgskko <msk724012@gmail.com>
2022-04-28 13:34:36 +0900

    second

--- praticjava.java
+++ praticjava.java
@@ -1,6 +1,10 @@
 public class praticjava {
     public static void main(String[] args) {
         String[] arr = {"명석","유빈","진혁"};
-        System.out.println(arr[0]);
+        for(int i =0 ; i < 3;i++){
+            System.out.println(arr[i]);
+
+        }
+        
         }
 }
