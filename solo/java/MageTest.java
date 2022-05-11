dcb0579902655360fdd908a254a20bf798165495
mgskko <msk724012@gmail.com>
2022-05-12 00:42:15 +0900

    0410

--- solo/java/MageTest.java
+++ solo/java/MageTest.java
@@ -1,6 +1,6 @@
 public class MageTest {
     public static void main(String[] args) {
-        Mage mage = new Mage("Mage the Magician1");
+        Mage mage = new Mage("Mage the Magician");
 
         mage.printStatus();
     }
